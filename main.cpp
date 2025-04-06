e68175fd3c2d29b226feb7a5067e42f02c0643ee
Joshua Mena <jmena023@ucr.edu>
2025-04-05 20:56:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,78 +1,86 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //begin lcd screen and set up at respective pins
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // LCD pin setup
 
-enum ButtonState {IDLE, PRESSED, RELEASED};
-enum ButtonState2 {IDLE, PRESSED, RELEASED};
+// Button pins
+const int buttonPin = 10;
+const int buttonPin2 = 8;
+
+// Button states
+enum ButtonState { IDLE, PRESSED, RELEASED };
 ButtonState buttonState = IDLE;
 ButtonState buttonState2 = IDLE;
 
-// basic setup for when device is turned on
+// Setup runs once
 void setup() {
-  const int buttonPin = 10;
-  const int buttonPin2 = 8;
-  Serial.begin(9600); //have serial sensor detect baud rate of 9600
-  lcd.begin(16, 2); //begin lcd display (16 columns and 2 rows)
-  lcd.setcursor(0,0);
-  lcd.println("Hello World")
-  lcd.print("by BearMax :D")
-  pinMode(buttonPin,INPUT_PULLUP);
-  pinMode(buttonPin2,INPUT_PULLUP);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World");
+  lcd.setCursor(0, 1);
+  lcd.print("by BearMax :D");
 
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
 }
 
+// Loop runs forever
 void loop() {
   static bool lastButtonState = HIGH;
   static bool lastButtonState2 = HIGH;
-  bool buttonStateNow = digitalRead (buttonPin);
-  bool buttonStateNow2 = digitalRead (buttonPin);
 
+  bool buttonStateNow = digitalRead(buttonPin);
+  bool buttonStateNow2 = digitalRead(buttonPin2);
+
+  // First Button
   switch (buttonState) {
     case IDLE:
-      if (buttonStateNow == LOW && lastButtonState == HIGH){
+      if (buttonStateNow == LOW && lastButtonState == HIGH) {
         buttonState = PRESSED;
       }
       break;
-
     case PRESSED:
-      if (buttonStateNow == LOW) {
+      if (buttonStateNow == HIGH) {
         buttonState = RELEASED;
-        lcd.print("Button1")
-      }
-      break;
-
-    case RELEASED:
-      if (buttonStateNow == HIGH){
-        buttonState = IDLE; // Ready for next press
         lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Button 1 Pressed");
+        Serial.println("Button 1 Pressed");
       }
       break;
-}
+    case RELEASED:
+      if (buttonStateNow == LOW) {
+        buttonState = IDLE;
+      }
+      break;
+  }
 
+  // Second Button
   switch (buttonState2) {
     case IDLE:
-      if (buttonStateNow2 == LOW && lastButtonState2 == HIGH){
+      if (buttonStateNow2 == LOW && lastButtonState2 == HIGH) {
         buttonState2 = PRESSED;
       }
       break;
-
     case PRESSED:
-      if (buttonStateNow2 == LOW) {
+      if (buttonStateNow2 == HIGH) {
         buttonState2 = RELEASED;
-        lcd.print("Button2")
-      }
-      break;
-
-    case RELEASED:
-      if (buttonStateNow2 == HIGH){
-        buttonState2 = IDLE; // Ready for next press
         lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Button 2 Pressed");
+        Serial.println("Button 2 Pressed");
       }
       break;
-}
+    case RELEASED:
+      if (buttonStateNow2 == LOW) {
+        buttonState2 = IDLE;
+      }
+      break;
+  }
 
-lastButtonState = buttonStateNow;
-lastButtonState2 = buttonStateNow2;
+  // Update previous states
+  lastButtonState = buttonStateNow;
+  lastButtonState2 = buttonStateNow2;
 }
 
 
